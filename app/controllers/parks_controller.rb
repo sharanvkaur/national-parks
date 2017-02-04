590052d40ae10c1afaafcf47e070eea41ba21a9e
Sharan Kaur <sharanvkaur@gmail.com>
2017-02-04 23:46:55 +0800

    part 1 done

---
+++ app/controllers/parks_controller.rb
@@ -0,0 +1,63 @@
+class ParksController < ApplicationController
+    before_action :set_park, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @parks = Park.all
+  end
+
+  def show
+  end
+
+  def new
+    @park = Park.new
+  end
+
+  def create
+      @park = Park.new(park_params)
+
+      respond_to do |format|
+        if @park.save
+          format.html { redirect_to @park, notice: 'Park was successfully created.' }
+          format.json { render :show, status: :created, location: @park }
+        else
+          format.html { render :new }
+          format.json { render json: @park.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+  def edit
+  end
+
+  def update
+  respond_to do |format|
+    if @park.update(park_params)
+      format.html { redirect_to @park, notice: 'Park was successfully updated.' }
+      format.json { render :show, status: :ok, location: @park }
+    else
+      format.html { render :edit }
+      format.json { render json: @park.errors, status: :unprocessable_entity }
+    end
+  end
+end
+
+  def destroy
+  @park.destroy
+  respond_to do |format|
+    format.html { redirect_to parks_url, notice: 'Park was successfully destroyed.' }
+    format.json { head :no_content }
+  end
+end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_park
+      @park = Park.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def park_params
+      params.require(:park).permit(:name, :description, :picture)
+    end
+
+end
---
+++ app/views/parks/_park.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! park, :id, :name, :description, :picture, :created_at, :updated_at, :age, :nuisance
+json.url park_url(park, format: :json)
